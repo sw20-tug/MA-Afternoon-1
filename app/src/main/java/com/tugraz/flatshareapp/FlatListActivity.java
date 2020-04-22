055bca398a65b543000360ebd9f43af55af5eab1
PoojaYeli <puja.v.y@gmail.com>
2020-04-22 16:53:19 +0200

    FLAT_003 fetch the flat details list [KC, YP]

--- app/src/main/java/com/tugraz/flatshareapp/FlatListActivity.java
+++ app/src/main/java/com/tugraz/flatshareapp/FlatListActivity.java
@@ -4,14 +4,17 @@ import android.os.Bundle;
 //import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.tugraz.flatshareapp.database.FlatRepository;
 import com.tugraz.flatshareapp.database.Models.Flat;
 
 import java.util.List;
 
-public class FlatListActivity  extends AppCompatActivity {
+public class FlatListActivity  extends FragmentActivity {
     FlatRepository flatData;
     String flatName;
     Boolean flatActive;
@@ -20,6 +23,14 @@ public class FlatListActivity  extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flat_list);
+
+        //
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FlatListDetailFragment fragment = new FlatListDetailFragment();
+        fragmentTransaction.add(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+
         try {
             List<Flat> allFlatdata = flatData.getAllFlats();
 
